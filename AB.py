ce869dec077165ffe8d1813e009f8fbf9a5b6c12
Alex O. Holcombe <aoholcombe@gmail.com>
2014-11-11 16:55:11 +1100

    Separation into NoiseStaircaseHelpers seems successful

--- AB.py
+++ AB.py
@@ -6,7 +6,8 @@ import psychopy.info
 import numpy as np
 from math import atan, log, ceil
 from copy import deepcopy
-import itertools, time, sys, os
+import time, sys, os
+from noiseStaircaseHelpers import printStaircase, createNoise
 
 tasks=['T1','T1T2']; task = tasks[1]
 refreshRate=60 #90 Hz used by Paolo  #set to the framerate of the monitor
@@ -309,100 +310,6 @@ def letterToNumber(letter): #A = 0, Z = 25
     except:
         return (-999)
 
-def printStaircaseStuff(staircase, briefTrialUpdate, alsoLog=False):
-    #if briefTrialUpdate, don't print everything, just the kind of stuff you like to know after each trial
-    #needs logging as a global variable, otherwise will fail when alsoLog=True
-    msg = 'staircase.data (incorrect/correct)=' + str(staircase.data)
-    print(msg)
-    if alsoLog:     logging.info(msg)
-    
-    if staircase.stepType == 'log':
-        msg = '\tstaircase.intensities (these are log intensities)=['
-        for i in range( len(staircase.intensities) ):
-            msg += '{:.2f} '.format(staircase.intensities[i])
-            #print('{:.2f} '.format(staircase.intensities[i]), end='') #I cant figure out a simpler way to prevent scientific notation
-        msg+= '], exponentiated=['
-        for j in range( len(staircase.intensities) ):
-            msg += '{:.2f} '.format(10**staircase.intensities[j])
-            #print('{:.2f} '.format(10**staircase.intensities[j]), end='') #I cant figure out a simpler way to prevent scientific notation
-        msg+= ']'
-        print(msg)
-        if alsoLog:     logging.info(msg)
-        #print(']')
-    else: #linear steps, so dont have to worry about log
-        msg = 'staircase.intensities =' + str( np.around(staircase.intensities,3) ) 
-        print(msg)
-        if alsoLog:     logging.info(msg)
-        
-    if type(staircase) is data.StairHandler:
-        numReversals = len(staircase.reversalIntensities)
-        msg= 'staircase number of reversals=' + str(numReversals) + '] '
-        msg+= 'reversal noiseProportions=' + str( 1- np.array(staircase.reversalIntensities) )
-        print(msg)
-        if alsoLog:     logging.info(msg)
-        if numReversals>0:
-            numReversalsToAvg = numReversals-1
-            msg= ('mean of final' + str(numReversalsToAvg) + 
-                      ' reversals =' + str( 1-np.average(staircase.reversalIntensities[-numReversalsToAvg:]) ) )
-            print(msg)
-            if alsoLog:     logging.info(msg)
-    elif type(staircase) is data.QuestHandler:
-            #some of below are private initialization variables I'm not really supposed to access
-            if not briefTrialUpdate:
-                msg= ('\tpThreshold (proportion correct for which trying to zero in on the corresponding parameter value) =' +
-                               str(staircase._quest.pThreshold) + '\n')
-                msg+= ('\tstopInterval (min 5-95% confidence interval required for  thresh  before stopping. If both this and nTrials is specified, whichever happens first)='+
-                               str(staircase.stopInterval) + '\n')
-                msg+= '\tstepType=' + str(staircase.stepType) + '\n'
-                msg+= '\tminVal=' + str(staircase.minVal) + '  maxVal=' + str(staircase.maxVal) + '\n'
-                msg+= '\tnTrials=' + str(staircase.nTrials)
-                print(msg)
-                if alsoLog:     logging.info(msg)
-
-    #below applies to both types of staircase
-    if staircase.thisTrialN == -1:
-        msg= 'thisTrialN = -1, suggesting you have not started it yet; need to call staircase.next()'
-        print(msg)
-        if alsoLog:     logging.info(msg)
-    else:
-        msg= 'staircase thisTrialN =' + str(staircase.thisTrialN)
-        print(msg)
-        if alsoLog:     logging.info(msg)
-        # staircase.calculateNextIntensity() sounds like something useful to get a preview of the next trial. Instead, seems to be 
-        #the internal function used to advance to the next trial.
-    
-def createNoiseArray(proportnNoise,fieldWidthPix,noiseColor): 
-    #noiseColor, assumes that colorSpace='rgb', triple between -1 and 1
-    numDots = int(proportnNoise*fieldWidthPix*fieldWidthPix)
-    if numDots ==0:
-        return None
-    #create a matrix of all possible pixel locations, shuffle it, pick off the first numDots ones
-    #0,0 is center of field
-    possibleXcoords = -fieldWidthPix/2 + np.arange(fieldWidthPix) 
-    possibleXcoords += fieldWidthPix/30 #adding one-tenth because for some mysterious reason not centered, I guess letters aren't drawn centered
-    possibleYcoords = deepcopy(possibleXcoords)
-    def expandgrid(*itrs):
-       product = list(itertools.product(*itrs))
-       return product
-    allFieldCoords = expandgrid(possibleXcoords,possibleYcoords)
-    #shuffle it
-    np.random.shuffle(allFieldCoords)
-    dotCoords = allFieldCoords[0:numDots]
-
-    #create opacity for each dot
-    opacs = np.ones(numDots)#all opaque
-    verticalAdjust = 3 #number of pixels to raise rectangle by. Using only uppercase letters and seem to be drawn above the line
-    noise = visual.ElementArrayStim(myWin,units='pix', elementTex=None, elementMask=None,
-        nElements=numDots, fieldSize=[fieldWidthPix,fieldWidthPix],
-        fieldPos=(0.0, verticalAdjust),
-        colorSpace='rgb',
-        colors=noiseColor, #set to black
-        xys= dotCoords, 
-        opacities=opacs,
-        sizes=1)
-    return (noise,allFieldCoords,numDots) #Can just use noise, but if want to generate new noise of same coherence level quickly, can just shuffle coords
-
-
 #print header for data file
 print('experimentPhase\ttrialnum\tsubject\ttask\t',file=dataFile,end='')
 print('noisePercent\t',end='',file=dataFile)
@@ -581,7 +488,7 @@ def do_RSVP_stim(cue1pos, cue2lag, proportnNoise,trialN):
     correctAnswers = np.array( letterSequence[cuesPos] )
     noise = None; allFieldCoords=None; numNoiseDots=0
     if proportnNoise > 0: #generating noise is time-consuming, so only do it once per trial. Then shuffle noise coordinates for each letter
-        (noise,allFieldCoords,numNoiseDots) = createNoiseArray(proportnNoise,noiseFieldWidthPix, bgColor)
+        (noise,allFieldCoords,numNoiseDots) = createNoise(proportnNoise,myWin,noiseFieldWidthPix, bgColor)
 
     preDrawStimToGreasePipeline = list() #I don't know why this works, but without drawing it I have consistent timing blip first time that draw ringInnerR for phantom contours
     cue.setLineColor(bgColor)
@@ -730,7 +637,7 @@ if doStaircase:
                 print('Readying to import ',corrEachTrial,' and intensities ',prefaceStaircaseNoise)
                 staircase.importData(100-prefaceStaircaseNoise, np.array(corrEachTrial)) 
             try: #advance the staircase
-                printStaircaseStuff(staircase, briefTrialUpdate=True, alsoLog=False)
+                printStaircase(staircase, briefTrialUpdate=True, alsoLog=False)
                 noisePercent = 100. - staircase.next()  #will step through the staircase, based on whether told it (addResponse) got it right or wrong
                 staircaseTrialN += 1
             except StopIteration: #Need this here, even though test for finished above. I can't understand why finished test doesn't accomplish this.
@@ -767,7 +674,7 @@ if doStaircase:
     timeAndDateStr = time.strftime("%H:%M on %d %b %Y", time.localtime())
     msg= ('ABORTED' if expStop else 'Finished') + ' staircase part of experiment at ' + timeAndDateStr
     logging.info(msg); print(msg)
-    printStaircaseStuff(staircase, briefTrialUpdate=False, alsoLog=True)
+    printStaircase(staircase, briefTrialUpdate=False, alsoLog=True)
     print('staircase.quantile=',round(staircase.quantile(),2),' sd=',round(staircase.sd(),2))
     threshNoise = 100- round(staircase.quantile(),3)
     threshNoise = max( 0, threshNoise ) #If get them all wrong, posterior peaks at a very negative number
